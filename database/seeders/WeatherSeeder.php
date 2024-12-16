d84f27f2ee96cb3e3a9db69368b88d06de4d3818
dukeduke00 <dusangostic69@gmail.com>
2024-12-16 16:38:33 +0100

    * dodavanje WeatherSeedera, grad se ne moze ponoviti

---
+++ database/seeders/WeatherSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\WeatherModel;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class WeatherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $grad = $this->command->getOutput()->ask("Unesite ime grada");
+        if($grad === null)
+        {
+            $this->command->getOutput()->error("Niste unijeli ime grada");
+            return;
+        }
+
+        $postojeciGrad = WeatherModel::where('town_name', $grad)->first();
+        if($postojeciGrad){
+            $this->command->getOutput()->error("Ovaj grad vec postoji u bazi");
+            return;
+        }
+
+        $temperatura = $this->command->getOutput()->ask("Unesite temperaturu");
+        if($temperatura === null)
+        {
+            $this->command->getOutput()->error("Niste unijeli temperaturu");
+            return;
+        }
+
+        WeatherModel::create([
+            'town_name' => $grad,
+            'temperature' => $temperatura,
+        ]);
+
+        $this->command->info("Uspjesno ste dodali grad i temperaturu.");
+    }
+}
