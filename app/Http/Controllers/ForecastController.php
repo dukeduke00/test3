ccfe3c3031d096c5ceab2371ec303600bd737450
dukeduke00 <dusangostic69@gmail.com>
2024-12-18 18:15:27 +0100

    * dodavanje Cities migracije

---
+++ app/Http/Controllers/ForecastController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ForecastController extends Controller
+{
+    public function index($city)
+    {
+        $forecasts = [
+            "doboj" => [7, 11, 5, 0, -1],
+            "sarajevo" => [5, 6, 0, -1, -3],
+        ];
+
+        $city = strtolower($city);
+
+       if(!array_key_exists($city, $forecasts))
+       {
+            die("Ovaj grad ne postoji");
+       }
+
+       dd($forecasts[$city]);
+
+    }
+
+
+}
