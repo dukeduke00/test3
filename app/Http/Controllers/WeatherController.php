346c827670fc504f251b84ffb79e4c6bc0d2f3e7
dukeduke00 <dusangostic69@gmail.com>
2024-12-23 18:51:02 +0100

    * Relationships between Cities and Forecasts

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WeatherModel;
+use Illuminate\Http\Request;
+
+class WeatherController extends Controller
+{
+    public function index()
+    {
+            $prognoza = WeatherModel::all();
+
+            return view('weather', compact('prognoza'));
+    }
+
+}
